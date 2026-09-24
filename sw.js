61fed0a2a02e3d04f3e6a52e879dc4ff5e52b579
Shashank Upadhyay <iamshashank1o1@gmail.com>
2026-09-24 14:53:26 +0530

    CV upload, minimum salary and skills-match filters

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="job-desk-v5";
+const CACHE="job-desk-v6";
 const CORE=["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png","icon-maskable-512.png","apple-touch-icon.png"];
 const CFG_KEY="./__jobdesk_cfg";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
